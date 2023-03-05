d0a2d41c0205db903f8ba7b16214f18025d91852
LaGa-Mi <108598112+LaGa-Mi@users.noreply.github.com>
2023-03-05 02:42:52 -0500

    Final_Edition

---
+++ Encrypter.c
@@ -0,0 +1,21 @@
+//Necessary libraries for program
+#include <stdio.h>
+#include <stdlib.h>
+
+//Max size of hash table
+#define MAX_CAPACITY 50000
+
+typedef struct employee{
+    unsigned int ID;
+    
+}
+
+typedef struct point{
+    char* key;
+
+}
+
+//Main function of program
+int main(void){
+
+}
